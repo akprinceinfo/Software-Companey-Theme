94d93245f594d7ac795d320d679dafdd55a3ae0e
akprinceinfo <akprince0268@gmail.com>
2024-09-21 22:31:43 +0600

    About Us section daynamic

--- template-parts/content-about.php
+++ template-parts/content-about.php
@@ -1,37 +1,68 @@
+ 
+ <?php 
+   
+    $about_title = get_field('about_title','options');
+    $about_sub_tittle = get_field('about_sub_tittle','options');
+    $about_description = get_field('about_description','options');
+    $about_contact_header = get_field('about_contact_header','options');
+    $about_contact_information = get_field('about_contact_information','options');
+    $about_contact_btn = get_field('about_contact_btn','options');
+    $about_contact_btn_link = get_field('about_contact_btn_link','options');
+    $about_image = get_field('about_image','options');
+    $about_info_subtitles = get_field('about_info_subtitle','options');
+   
+
+ ?>
+ 
+ 
  <!-- About Start -->
  <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
         <div class="container py-5">
             <div class="row g-5">
                 <div class="col-lg-7">
                     <div class="section-title position-relative pb-3 mb-5">
-                        <h5 class="fw-bold text-primary text-uppercase">About Us</h5>
-                        <h1 class="mb-0">The Best IT Solution With 10 Years of Experience</h1>
+                        <h5 class="fw-bold text-primary text-uppercase"><?php echo $about_title ;?></h5>
+                        <h1 class="mb-0"><?php echo $about_sub_tittle ;?></h1>
                     </div>
-                    <p class="mb-4">Tempor erat elitr rebum at clita. Diam dolor diam ipsum et tempor sit. Aliqu diam amet diam et eos labore. Clita erat ipsum et lorem et sit, sed stet no labore lorem sit. Sanctus clita duo justo et tempor eirmod magna dolore erat amet</p>
+                    <p class="mb-4"><?php echo $about_description ;?></p>
                     <div class="row g-0 mb-3">
+
+                        <?php
+                            foreach($about_info_subtitles as $about_info_subtitle){
+                        ?>
+
                         <div class="col-sm-6 wow zoomIn" data-wow-delay="0.2s">
-                            <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Award Winning</h5>
-                            <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Professional Staff</h5>
+                            <h5 class="mb-3"><i class="<?php echo $about_info_subtitle['about_info_subtitle_icone'] ;?> text-primary me-3"></i><?php echo $about_info_subtitle['about_info_subtitle_header'] ;?></h5>
                         </div>
-                        <div class="col-sm-6 wow zoomIn" data-wow-delay="0.4s">
-                            <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>24/7 Support</h5>
-                            <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i>Fair Prices</h5>
-                        </div>
+
+                        <?php
+
+                            }
+                        ?>
+
+                        
+
+
+
+                        <!-- <div class="col-sm-6 wow zoomIn" data-wow-delay="0.4s">
+                            <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i></h5>
+                            <h5 class="mb-3"><i class="fa fa-check text-primary me-3"></i></h5>
+                        </div> -->
                     </div>
                     <div class="d-flex align-items-center mb-4 wow fadeIn" data-wow-delay="0.6s">
                         <div class="bg-primary d-flex align-items-center justify-content-center rounded" style="width: 60px; height: 60px;">
                             <i class="fa fa-phone-alt text-white"></i>
                         </div>
                         <div class="ps-4">
-                            <h5 class="mb-2">Call to ask any question</h5>
-                            <h4 class="text-primary mb-0">+012 345 6789</h4>
+                            <h5 class="mb-2"><?php echo $about_contact_header ;?></h5>
+                            <h4 class="text-primary mb-0"><?php echo $about_contact_information ;?></h4>
                         </div>
                     </div>
-                    <a href="quote.html" class="btn btn-primary py-3 px-5 mt-3 wow zoomIn" data-wow-delay="0.9s">Request A Quote</a>
+                    <a href="<?php echo $about_contact_btn_link ;?>" class="btn btn-primary py-3 px-5 mt-3 wow zoomIn" data-wow-delay="0.9s"><?php echo $about_contact_btn ;?></a>
                 </div>
                 <div class="col-lg-5" style="min-height: 500px;">
                     <div class="position-relative h-100">
-                        <img class="position-absolute w-100 h-100 rounded wow zoomIn" data-wow-delay="0.9s" src="<?php echo get_template_directory_uri() ?>/img/about.jpg" style="object-fit: cover;">
+                        <img class="position-absolute w-100 h-100 rounded wow zoomIn" data-wow-delay="0.9s" src="<?php echo $about_image['url'] ;?>" style="object-fit: cover;">
                     </div>
                 </div>
             </div>
