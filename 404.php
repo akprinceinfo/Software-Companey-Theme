85d1bccb44c2bc7760d89e461cb7e2ba41e52762
akprinceinfo <akprince0268@gmail.com>
2024-09-23 11:41:09 +0600

    404 Page add

---
+++ 404.php
@@ -0,0 +1,28 @@
+<?php get_header(); ?>
+
+    
+    <!-- header section  -->
+    <div class="container-fluid bg-primary py-5 bg-header" style="margin-bottom: 90px;">
+        <div class="row py-5">
+            <div class="col-12 pt-lg-5 mt-lg-5 text-center">
+                <h1 class="display-4 text-white animated zoomIn">
+                    404 Not Found
+                </h1>
+                
+            </div>
+            </div>
+    </div>
+    
+    <section class="404">
+        <h1 class="text-center m-5">404 Not Found</h1>
+    </section>
+
+
+
+
+
+
+
+
+
+<?php get_footer();?>
